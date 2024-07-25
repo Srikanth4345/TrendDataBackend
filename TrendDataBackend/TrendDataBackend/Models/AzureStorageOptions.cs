bf5d96b5c4a879db04c57a763ce0b7817f3882fb
Phemanth85 <102746578+Phemanth85@users.noreply.github.com>
2024-07-25 11:26:17 +0000

    Delete TrendDataBackend directory

--- TrendDataBackend/TrendDataBackend/Models/AzureStorageOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TrendDataBackend.Models
-{
-    public class AzureStorageOptions
-    {
-        public string ConnectionString { get; set; }
-        public string TableName { get; set; }
-    }
-}
